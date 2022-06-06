003e5ffde933c730d322f2768c8e3dd9d0fcfe58
LyndelC <106130930+LyndelC@users.noreply.github.com>
2022-06-06 20:27:23 +1000

    triangle perimeter

---
+++ fileread.py
@@ -0,0 +1,8 @@
+# find perimeter of triangle
+# add side1, side2, and side 3
+# display result
+side1 = float(input("enter length of side 1: "))
+side2 = float(input("enter length of side 2: "))
+side3 = float(input("emter length of side 3: "))
+p = (side1 + side2 + side3)
+print("the perimeter of the triangle is ", p)
